c5cabf424319106ca6f76a5bd8fe04d5a9fb8c17
d-002 <minecraftenjoy@outlook.fr>
2025-10-15 09:19:05 +0200

    added annotations like needs testing, author favorite

--- i3lock/app.js
+++ i3lock/app.js
@@ -2,5 +2,8 @@ import { RainManager } from "./rain.js";
 
 // matrix rain
 const manager = new RainManager(document.getElementById("matrix"));
-manager.update();
-clearInterval(manager.interval);
+
+if (false) {
+    manager.update();
+    clearInterval(manager.interval);
+}
