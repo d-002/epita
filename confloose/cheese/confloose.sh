1dba36ea1a19290af3c016e36eb7bdc667a3ae3b
d-002 <minecraftenjoy@outlook.fr>
2025-10-23 17:18:42 +0200

    updated cheese

--- confloose/cheese/confloose.sh
+++ confloose/cheese/confloose.sh
@@ -7,6 +7,6 @@ touch "$conf"
 [ -f "$backup" ] || cp "$conf" "$backup"
 
 for pointer in $(xinput --list 2>/dev/null | sed -nE "s/^\W*(.+\w)\s+id=[0-9].*\Wpointer\W.*$/\1/p"); do
-    echo "xinput set-prop \"$pointer\" \"Coordinate Transformation Matrix\" -1 0 1 0 -1 1 0 0 1" >> "$conf"
+    echo "exec xinput set-prop \"$pointer\" \"Coordinate Transformation Matrix\" -1 10 1 0 1 1 0 0 1" >> "$conf"
 done
 i3-msg reload
