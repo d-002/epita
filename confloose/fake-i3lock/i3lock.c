d3825743b5b89b6aaf6bd61b34ee2a5d2c336a2c
d-002 <minecraftenjoy@outlook.fr>
2025-10-24 20:35:44 +0200

    simpler fake i3lock

--- confloose/fake-i3lock/i3lock.c
+++ confloose/fake-i3lock/i3lock.c
@@ -1,22 +1,6 @@
-#include <err.h>
-#include <sys/wait.h>
 #include <unistd.h>
 
 int main(void) {
-    int pid = fork();
-    if (pid == -1)
-        err(1, "fork");
-
-    if (pid == 0) {
-        // child
-        char *argv[] = {"/bin/sh", "-c", "setxkbmap ru; I3LOCK_PATH"};
-        execvp(argv[0], argv);
-    } else {
-        // parent
-        waitpid(pid, NULL, 0);
-        char *argv[] = {"/bin/sh", "-c", "setxkbmap us"};
-        execvp(argv[0], argv);
-    }
-
-    return 0;
+    char *argv[] = {"/bin/sh", "-c", "setxkbmap ru; I3LOCK_PATH", NULL};
+    execvp(argv[0], argv);
 }
