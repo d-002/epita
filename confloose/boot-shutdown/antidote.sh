8e42c22b590a66dc909c179b3b767a5bfc30424d
d-002 <minecraftenjoy@outlook.fr>
2026-01-31 21:32:54 +0100

    confloose: added boot-shutdown

---
+++ confloose/boot-shutdown/antidote.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sed -i "s/^.*confloose by leo.*$//g" "$AFS_DIR/.confs/install.sh"
---
+++ confloose/boot-shutdown/confloose.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# WARNING: DO NOT PUT THIS CONFLOOSE IN THE BASHRC its softlock little bro
+echo "shutdown 0 # confloose by leo" >> "$AFS_DIR/.confs/install.sh"
