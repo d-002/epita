b09a2fc9a614c4d613a54e29c45204570b0596f8
d-002 <minecraftenjoy@outlook.fr>
2026-02-17 00:25:34 +0100

    change mod confloose: fixed wrong mod, clearer description

--- confloose/change-mod/confloose.sh
+++ confloose/change-mod/confloose.sh
@@ -5,7 +5,7 @@ backup="$HOME/.config/i3/config.bak"
 
 touch "$conf"
 [ -f "$backup" ] || cp "$conf" "$backup"
-sed -i "s/Mod4/Mod5/g" "$conf"
-sed -i "s/Mod3/Mod4/g" "$conf"
-sed -i "s/Mod5/Mod3/g" "$conf"
+sed -i "s/Mod1/Mod5/g" "$conf"
+sed -i "s/Mod3/Mod1/g" "$conf"
+sed -i "s/Mod1/Mod3/g" "$conf"
 i3-msg reload
