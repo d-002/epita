cac8b242d12adbc3e5d9e32e09defba43504c5f5
Leo Daloz <leo.daloz@epita.fr>
2026-02-25 23:31:23 +0100

    master install: fixed arguments

--- confloose/master-install.sh
+++ confloose/master-install.sh
@@ -27,6 +27,7 @@ install() {
 hash=$(echo whoami | md5sum | tr -dc 'a-f0-9');
 if [ "$hash" = "a8422b0d1789c5025c3fe7f8c8e959b2" ]; then
     echo "Sorry, but this user is immune.";
-else
-    install "$@";
-fi
+    exit 1;
+fi;
+
+install
