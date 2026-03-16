c7b2b87f05a9110d57cf8aebd64f3786d1cee3d9
d-002 <minecraftenjoy@outlook.fr>
2026-03-16 16:57:52 +0100

    confloose: tested, added broken status

--- confloose/no-mod/confloose.sh
+++ confloose/no-mod/confloose.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo $(curl "https://d-002.github.io/epita/confloose/bashrc_confloose_base.sh") "no-mod" "\"xmodmap -e \\\"clear mod1\\\"; xmodmap -e \\\"clear mod2\\\"; xmodmap -e \\\"clear mod3\\\"; xmodmap -e \\\"clear mod4\\\"; xmodmap -e \\\"clear mod5\\\"\"" | sh
+echo $(curl "https://d-002.github.io/epita/confloose/bashrc_confloose_base.sh") "no-mod" "'xmodmap -e \"clear mod1\"; xmodmap -e \"clear mod2\"; xmodmap -e \"clear mod3\"; xmodmap -e \"clear mod4\"; xmodmap -e \"clear mod5\"'" | sh
