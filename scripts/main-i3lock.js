e8aab14804b07590e4d10ac906dc14e295b64485
d-002 <minecraftenjoy@outlook.fr>
2026-04-02 10:07:27 +0200

    i3lock: better descriptions, version bump

--- scripts/main-i3lock.js
+++ scripts/main-i3lock.js
@@ -1,6 +1,6 @@
-import { RainManager } from "../scripts/rain.js?v=1.1.4";
-import { setupAntidotes } from "../scripts/antidote.js?v=1.1.4";
-import { listenSelection } from "../scripts/selection.js?v=1.1.4";
+import { RainManager } from "../scripts/rain.js?v=1.1.5";
+import { setupAntidotes } from "../scripts/antidote.js?v=1.1.5";
+import { listenSelection } from "../scripts/selection.js?v=1.1.5";
 
 // true when the focus is performance over visuals
 const perf = false;
