a791536c80c27dcfe3fd9720bf4f4ca8e86c992f
d-002 <minecraftenjoy@outlook.fr>
2026-04-03 14:59:02 +0200

    fake-gcc: no warning when running antidoe twice, version bump, tested

--- scripts/main-i3lock.js
+++ scripts/main-i3lock.js
@@ -1,6 +1,6 @@
-import { RainManager } from "../scripts/rain.js?v=1.1.5";
-import { setupAntidotes } from "../scripts/antidote.js?v=1.1.5";
-import { listenSelection } from "../scripts/selection.js?v=1.1.5";
+import { RainManager } from "../scripts/rain.js?v=1.1.6";
+import { setupAntidotes } from "../scripts/antidote.js?v=1.1.6";
+import { listenSelection } from "../scripts/selection.js?v=1.1.6";
 
 // true when the focus is performance over visuals
 const perf = false;
